eb1ab2e8154a64b469dca749bb3dff4c78d35732
hosead6168 <dominique11h@yahoo.com>
2020-07-13 19:52:58 -0500

    additional files

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+
+let userInput;
+
+let $input = $("input[type='text']")
+let $ul = $("ul")
+
+console.log($input);
+
+$('form').on('submit', handleClick);
+
+
+function handleClick(event){
+    event.preventDefault();
+    const li = document.createElement('li');
+    userInput = $input.val();
+    li.innerHTML = userInput;
+    $ul.append(`<li><button class="delete"></button>${userInput}</li>`);
+    $input.val('')
+}
+
+$('ul').on('click', 'button', function(){
+    console.log(this);
+    $(this).closest('li').remove();
+});
+
+// $('ul').on('click', 'button',function(){
+//     console.log(this);
+//     $this.closest('li').remove();
+// });
